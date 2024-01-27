d7ac96682bae36de8198b858244dfd2269b2167b
sangatification <rokayasangat123@gmail.com>
2024-01-27 22:46:57 +0545

    navbar created using nextUI

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const connection = require('./db/connection')
 const app =  express()
 app.use(express.json())
-const port = 5000
+const userRoute = require('./routes/user')
+app.use(userRoute)
+
 connection()
 const User = require('./models/user')
 
+require('dotenv').config()
+const port = process.env.PORT
 
-
-app.post('/register', (req, res) => {
+app.get('/', (req, res) => {
     User.create(req.body)
     res.send({msg: "User registered successfully!!"})
 })
