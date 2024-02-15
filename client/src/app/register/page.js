1567d1da5bb256208f0a48f4abd9b879344c2d69
sangatification <rokayasangat123@gmail.com>
2024-02-15 16:00:00 +0545

    css manipulations in reigster page

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -50,50 +50,60 @@ const SignupForm = () => {
     <form className={styles.formFields} onSubmit={formik.handleSubmit}>
       <div className={styles.cardPosition}>
         <Card className={styles.formCard}>
+          <h3 className='font-bold m-2 text-xl'>Sign Up</h3>
+          <p className='text-xs my-1'>Enter sign up details to get registered</p>
           <CardBody>
+            <h1>Full Name</h1>
             <Input id="fullName"
+              className='text-1 font-thin italic'
               name="fullName"
               type="text"
               onChange={formik.handleChange}
-              value={formik.values.fullName} label="Full Name" />
+              value={formik.values.fullName} label="Enter your full name" />
             {formik?.errors.fullName}
-            <br />
+            <br/>
+            <h1>Email</h1>
             <Input id="email"
+              className='text-1 font-thin italic'
               name="email"
               type="email"
               onChange={formik.handleChange}
-              value={formik.values.email} label="Email" />
+              value={formik.values.email} label="Enter your email" />
             {formik?.errors.email}
-            <br />
+            <br/>
+            <h1>Phone Number</h1>
             <Input id="phoneNumber"
+              className='text-1 font-thin italic'
               name="phoneNumber"
               type="text"
               onChange={formik.handleChange}
-              value={formik.values.phoneNumber} label="Phone Number" />
+              value={formik.values.phoneNumber} label="Enter your phone number" />
             {formik?.errors.phoneNumber}
-            <br />
+            <br/>
+            <h1>Role</h1>
             <Input id="role"
+              className='text-1 font-thin italic'
               name="role"
               type="text"
               onChange={formik.handleChange}
-              value={formik.values.role} label="Role" />
+              value={formik.values.role} label="Select your role" />
             {formik?.errors.role}
-            <br />
+            <br/>
+            <h1>Password</h1>
             <Input id="password"
+              className='text-1 font-thin italic'
               name="password"
               type="password"
               onChange={formik.handleChange}
-              value={formik.values.password} label="Password" />
+              value={formik.values.password} label="Enter your password" />
             {formik?.errors.password}
-            <br />
-            <button className={styles.btn} type="submit">Submit</button>
+            <br/>
+            <div className={styles.btn} >
+            <button type="submit">Submit</button>
+            </div>
           </CardBody>
         </Card>
       </div>
-
-
-
-
     </form>
   );
 };
