84e2ae1a73af4285cdfabfaa14c2f4d5c419b68d
sangatification <rokayasangat123@gmail.com>
2024-02-17 15:33:17 +0545

    backend for unique phone number only in register page

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -26,11 +26,13 @@ const SignupForm = () => {
   });
 
   const reigsterUser = async (values) => {
-    await fetch('http://localhost:5000/register/', {
+    const res = await fetch('http://localhost:5000/register/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(values)
     })
+    const data = await res.json()
+    alert(data.msg)
   }
 
   const formik = useFormik({
--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,9 +1,20 @@
 const User = require('../models/user')
 const registerNewUser = async(req, res) => {
-    await User.create(req.body)
-    res.json({
-        msg: "registered successfully"
-    })
+    try{
+        const existingUser = await User.findOne({phoneNumber: req.body.phoneNumber});
+        if (existingUser) {
+            return res.status(403).json({
+                msg: "Phone number already exist"
+            })
+        }
+        await User.create(req.body)
+        res.json({
+            msg: "registered successfully"
+        })
+    }catch(err){
+        console.log(err)
+    }
+
 }
 
 module.exports = { registerNewUser }
