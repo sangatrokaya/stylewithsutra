b514eeb67e99b777295c6aded2697a830b25f6d3
sangatification <timetosangatify66@gmail.com>
2024-02-24 10:42:59 +0545

    footer adjustments

--- client/src/components/footer/page.js
+++ client/src/components/footer/page.js
@@ -22,84 +22,84 @@ const page = () => {
           <div className="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-orange-500 tracking-widest text-lg mb-3">SHOP MEN</h2>
-              <nav className="list-none mb-10">
+              <nav className="list-none mb-10 text-sm">
                 <li>
-                  <a className="text-white hover:text-gray-800">General Fashion</a>
+                  <a className="text-white hover:text-orange-500">General Fashion</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Winter</a>
+                  <a className="text-white hover:text-orange-500">Winter</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Summer</a>
+                  <a className="text-white hover:text-orange-500">Summer</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Formal</a>
+                  <a className="text-white hover:text-orange-500">Formal</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Casual</a>
+                  <a className="text-white hover:text-orange-500">Casual</a>
                 </li>
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-orange-500 tracking-widest text-lg mb-3">SHOP WOMEN</h2>
-              <nav className="list-none mb-10">
+              <nav className="list-none mb-10 text-sm">
                 <li>
-                  <a className="text-white hover:text-gray-800">General Fashion</a>
+                  <a className="text-white hover:text-orange-500">General Fashion</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Winter</a>
+                  <a className="text-white hover:text-orange-500">Winter</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Summer</a>
+                  <a className="text-white hover:text-orange-500">Summer</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Formal</a>
+                  <a className="text-white hover:text-orange-500">Formal</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Casual</a>
+                  <a className="text-white hover:text-orange-500">Casual</a>
                 </li>
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-orange-500 tracking-widest text-lg mb-3">SHOP CHILDREN</h2>
-              <nav className="list-none mb-10">
+              <nav className="list-none mb-10 text-sm">
                 <li>
-                  <a className="text-white hover:text-gray-800">General Fashion</a>
+                  <a className="text-white hover:text-orange-500">General Fashion</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Winter</a>
+                  <a className="text-white hover:text-orange-500">Winter</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Summer</a>
+                  <a className="text-white hover:text-orange-500">Summer</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Formal</a>
+                  <a className="text-white hover:text-orange-500">Formal</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Casual</a>
+                  <a className="text-white hover:text-orange-500">Casual</a>
                 </li>
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-orange-500 tracking-widest text-lg mb-3">QUICK LINKS</h2>
-              <nav className="list-none mb-10">
+              <nav className="list-none mb-10 text-sm">
                 <li>
-                  <a className="text-white hover:text-gray-800">Track Your Order</a>
+                  <a className="text-white hover:text-orange-500">Track Your Order</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Suppport</a>
+                  <a className="text-white hover:text-orange-500">Suppport</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">FAQ</a>
+                  <a className="text-white hover:text-orange-500">FAQ</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Career</a>
+                  <a className="text-white hover:text-orange-500">Career</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">About</a>
+                  <a className="text-white hover:text-orange-500">About</a>
                 </li>
                 <li>
-                  <a className="text-white hover:text-gray-800">Contact Us</a>
+                  <a className="text-white hover:text-orange-500">Contact Us</a>
                 </li>
               </nav>
             </div>
@@ -112,23 +112,23 @@ const page = () => {
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
               <a className="text-orange-500" href='https://www.facebook.com/stylewithsutra' target="_blank">
-                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5 hover:text-white" viewBox="0 0 24 24">
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
               </a>
               <a className="ml-3 text-orange-500">
-                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5 hover:text-white" viewBox="0 0 24 24">
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </a>
               <a className="ml-3 text-orange-500" href='https://www.instagram.com/stylewithsutra/' target='_blank'>
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5 hover:text-white" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
               </a>
               <a className="ml-3 text-orange-500">
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5 hover:text-white" viewBox="0 0 24 24">
                   <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                   <circle cx="4" cy="4" r="2" stroke="none"></circle>
                 </svg>
